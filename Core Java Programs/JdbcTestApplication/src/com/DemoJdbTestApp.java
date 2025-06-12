7560dd8ab711425eb4045cfc71145a37aa72017d
Akash <akash300383@gmail.com>
2025-06-12 14:41:13 +0530

    done

--- Core Java Programs/JdbcTestApplication/src/com/DemoJdbTestApp.java
+++ Core Java Programs/JdbcTestApplication/src/com/DemoJdbTestApp.java
@@ -8,6 +8,15 @@ public class DemoJdbTestApp {
 			System.out.println("Driver loaded...");
 Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/prolim_db", "root", "root@123");
 System.out.println("Connected to db");
+		Statement stmt = con.createStatement();
+		
+		
+		// insert Query 
+		int temp= stmt.executeUpdate("insert into product values(103,'Laptop',97000)");
+		if(temp>0) {
+			System.out.println("Record inserted successfully");
+		}
+		
 		} catch (Exception e) {
 			System.err.println(e);
 		}
