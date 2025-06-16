6c7e1a28d34e8e86a8007b1f42b959bcbb82c1a1
Akash <akash300383@gmail.com>
2025-06-16 14:49:53 +0530

    done

---
+++ Course3 - Spring Framework/Spring Framework/DIUsingXMLConfiguration/src/main/java/com/main/DemoTest.java
@@ -0,0 +1,13 @@
+package com.main;
+
+import com.bean.Employee;
+
+public class DemoTest {
+
+	public static void main(String[] args) {
+		Employee emp1 = new Employee();
+		emp1.display();
+
+	}
+
+}
