290606b7d016f0b298943c0e641abea66535accb
Akash <akash300383@gmail.com>
2025-06-16 15:27:01 +0530

    done

--- Course3 - Spring Framework/Spring Framework/DIUsingXMLConfiguration/src/main/java/com/bean/Employee.java
+++ Course3 - Spring Framework/Spring Framework/DIUsingXMLConfiguration/src/main/java/com/bean/Employee.java
@@ -1,7 +1,10 @@
 package com.bean;
 
 public class Employee {
-	
+private int id;
+private String name;
+private float salary;
+
 	public Employee() {
 		super();
 		System.out.println("object created...");
@@ -10,4 +13,10 @@ public class Employee {
 	public void display() {
 		System.out.println("Employee class display method");
 	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", salary=" + salary + "]";
+	}
+	
 }
