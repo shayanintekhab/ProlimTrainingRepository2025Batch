08e54d380289f5a37562c384d27386f7cab59445
Akash <akash300383@gmail.com>
2025-06-18 16:43:47 +0530

    done

--- Course3 - Spring Framework/Spring Framework/JPACrudOperation/src/main/java/com/bean/DemoTest.java
+++ Course3 - Spring Framework/Spring Framework/JPACrudOperation/src/main/java/com/bean/DemoTest.java
@@ -1,9 +1,12 @@
 package com.bean;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 
 public class DemoTest {
 
@@ -42,15 +45,31 @@ public class DemoTest {
 //		}
 	
 	// update query 
-	Product p = manager.find(Product.class, 101);
-	if(p==null) {
-		System.out.println("Record not present so didn't update");
-	}else {
-		tran.begin();
-			p.setPrice(55000);
-			manager.merge(p);		// update query 	
-		tran.commit();
-		System.out.println("Record updated");
+//	Product p = manager.find(Product.class, 101);
+//	if(p==null) {
+//		System.out.println("Record not present so didn't update");
+//	}else {
+//		tran.begin();
+//			p.setPrice(55000);
+//			manager.merge(p);		// update query 	
+//		tran.commit();
+//		System.out.println("Record updated");
+//	}
+	
+	// retrieve only one record using primary key 
+//	Product p = manager.find(Product.class, 102);
+//	if(p==null) {
+//		System.out.println("Record not present");
+//	}else {
+//		System.out.println(p);     // it call toString method to display output 
+//	}
+	
+	// retrieve all records using JPQL 
+	Query qry = manager.createQuery("select p from Product p");
+	List<Product> listOfProduct= qry.getResultList();
+	System.out.println("number of records are "+listOfProduct.size());
+	for(Product p : listOfProduct) {
+		System.out.println(p);
 	}
 	}
 
