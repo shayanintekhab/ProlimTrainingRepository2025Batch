0df5c3020f6079fbd75218af84ed90091a741ef0
Akash <akash300383@gmail.com>
2025-06-18 16:26:06 +0530

    done

--- Course3 - Spring Framework/Spring Framework/JPACrudOperation/src/main/java/com/bean/DemoTest.java
+++ Course3 - Spring Framework/Spring Framework/JPACrudOperation/src/main/java/com/bean/DemoTest.java
@@ -17,16 +17,41 @@ public class DemoTest {
 	EntityManager manager = emf.createEntityManager();
 	
 	Product p1 = new Product();
-	p1.setPid(100);
-	p1.setPname("TV");
-	p1.setPrice(56000);
+	p1.setPid(102);
+	p1.setPname("Laptop");
+	p1.setPrice(98000);
 	
 	EntityTransaction tran = manager.getTransaction();
+	// insert query 
+//		tran.begin();
+//		manager.persist(p1);       // save or insert record. 
+//		tran.commit();
+//		System.out.println("Record saved...");
 	
+	
+	// Delete the Record 
+	
+//		Product p = manager.find(Product.class, 100);
+//		if(p==null) {
+//			System.out.println("Record not present so didn't delete");
+//		}else {
+//			tran.begin();
+//				manager.remove(p);			// like delete query 
+//			tran.commit();
+//			System.out.println("Record deleted");
+//		}
+	
+	// update query 
+	Product p = manager.find(Product.class, 101);
+	if(p==null) {
+		System.out.println("Record not present so didn't update");
+	}else {
 		tran.begin();
-		manager.persist(p1);       // save or insert record. 
+			p.setPrice(55000);
+			manager.merge(p);		// update query 	
 		tran.commit();
-		System.out.println("Record saved...");
+		System.out.println("Record updated");
+	}
 	}
 
 }
