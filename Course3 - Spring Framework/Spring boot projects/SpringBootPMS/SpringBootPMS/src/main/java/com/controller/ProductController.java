711e70275beede361b1da4d2a36416b18d89a53b
Akash <akash300383@gmail.com>
2025-06-20 17:03:58 +0530

    done

--- Course3 - Spring Framework/Spring boot projects/SpringBootPMS/SpringBootPMS/src/main/java/com/controller/ProductController.java
+++ Course3 - Spring Framework/Spring boot projects/SpringBootPMS/SpringBootPMS/src/main/java/com/controller/ProductController.java
@@ -1,11 +1,14 @@
 package com.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.bean.Product;
 import com.service.ProductService;
 
 @Controller
@@ -21,4 +24,20 @@ public class ProductController {
 		mm.addAttribute("info", "Welcome to Product Management System");
 		return "index";
 	}
+	
+	@RequestMapping(value = "/addProductPage",method = RequestMethod.GET)
+	public String addProductPageOpen(Model mm) {  // DI for Model API for of spring mvc 
+		mm.addAttribute("info", "Welcome to Product Management System");
+		return "addProduct";
+	}
+	
+	@RequestMapping(value = "/viewProductPage",method = RequestMethod.GET)
+	public String viewPageOpen(Model mm) {  // DI for Model API for of spring mvc 
+		mm.addAttribute("info", "Welcome to Product Management System");
+		List<Product> listOfProduct = productService.findAllProducts();
+		mm.addAttribute("products", listOfProduct); 
+		return "viewProduct";
+	}
 }
+
+
