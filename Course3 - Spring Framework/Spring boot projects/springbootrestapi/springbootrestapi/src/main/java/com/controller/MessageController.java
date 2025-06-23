6e63173f50a9032abdd32b2573e70c7c178d7363
Akash <akash300383@gmail.com>
2025-06-23 14:53:44 +0530

    done

--- Course3 - Spring Framework/Spring boot projects/springbootrestapi/springbootrestapi/src/main/java/com/controller/MessageController.java
+++ Course3 - Spring Framework/Spring boot projects/springbootrestapi/springbootrestapi/src/main/java/com/controller/MessageController.java
@@ -1,6 +1,8 @@
 package com.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +14,17 @@ public class MessageController {
 	@RequestMapping(value = "hi")
 	public String sayHi() {
 		return "Welcome to Spring boot with plain text format";
+	}
+	@RequestMapping(value = "text",method = RequestMethod.GET,produces = MediaType.TEXT_PLAIN_VALUE)
+	public String sayPlainText() {
+		return "<h1>Welcome to Spring boot</h1>";
+	}
+	@RequestMapping(value = "html",method = RequestMethod.GET,produces = MediaType.TEXT_HTML_VALUE)
+	public String sayHtml() {
+		return "<h1>Welcome to Spring boot</h1>";
+	}
+	@RequestMapping(value = "xml",method = RequestMethod.GET,produces = MediaType.TEXT_XML_VALUE)
+	public String sayXml() {
+		return "<abc>Welcome to Spring boot</abc>";
 	}
 }
