23e89c42b4f832b25b5e7c7661791a49798ae0e1
Akash <akash300383@gmail.com>
2025-06-24 16:39:14 +0530

    done

--- Course3 - Spring Framework/Spring boot projects/ProductAndOrdersRestAPI/ProductAndOrdersRestAPI/src/main/java/com/main/ProductAndOrdersRestApiApplication.java
+++ Course3 - Spring Framework/Spring boot projects/ProductAndOrdersRestAPI/ProductAndOrdersRestAPI/src/main/java/com/main/ProductAndOrdersRestApiApplication.java
@@ -3,9 +3,11 @@ package com.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = "com")
-@EntityScan(basePackages = "com.bean")
+@SpringBootApplication(scanBasePackages = "com")	// Enable @RestController, @Service, @Repository annotation 
+@EntityScan(basePackages = "com.bean")				// Enable @Entity annotation part of jpa 
+@EnableJpaRepositories(basePackages = "com.repository") // Enable spring Jpa Data Features 
 public class ProductAndOrdersRestApiApplication {
 
 	public static void main(String[] args) {
