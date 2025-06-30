d1ddcacc277a84ee2bf91cdf5b3c14e66a40fbca
Akash <akash300383@gmail.com>
2025-06-30 15:58:49 +0530

    done

--- Course 4 - Develop a Web Application using frontend stack/JavaScript Using ES6/typesoffunctions.js
+++ Course 4 - Develop a Web Application using frontend stack/JavaScript Using ES6/typesoffunctions.js
@@ -11,3 +11,17 @@ let display3 = function() {
     document.writeln("<br/>Expression style function creation")
 }
 display3();
+let display4 = ()=>document.writeln("<br/> Simple Arrow function")
+display4();
+
+let addNumber1 = function(a,b){
+    var sum = a+b;
+    return sum;
+}
+document.writeln("<br> Sum of two number using expression stye is "+addNumber1(100,200))
+let addNumber2 = (a,b)=>a+b;        // arrow function return the value without return keyword. 
+document.writeln("<br> sum of two number using arrow style is "+addNumber2(100,200));
+
+
+
+
