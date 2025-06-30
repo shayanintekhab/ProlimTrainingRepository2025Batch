430145ed5211c6c232b468eb5570105ed8627b89
Akash <akash300383@gmail.com>
2025-06-30 16:32:01 +0530

    done

--- Course 4 - Develop a Web Application using frontend stack/JavaScript Using ES6/typesoffunctions.js
+++ Course 4 - Develop a Web Application using frontend stack/JavaScript Using ES6/typesoffunctions.js
@@ -22,6 +22,21 @@ document.writeln("<br> Sum of two number using expression stye is "+addNumber1(1
 let addNumber2 = (a,b)=>a+b;        // arrow function return the value without return keyword. 
 document.writeln("<br> sum of two number using arrow style is "+addNumber2(100,200));
 
-
-
-
+// expression style function to find the largest of two numbers 
+let findLargest1 = function(a,b) {
+    if(a>b){
+        return "a is largest"
+    }else {
+        return "b is largest"
+    }
+}
+// arrow style function to find the largest of two numbers 
+let findLargest2 = (a,b)=> {
+    if(a>b){
+        return "a is largest"
+    }else {
+        return "b is largest"
+    }
+}
+document.writeln("<br/> Largest of two number using expression style "+findLargest1(100,50))
+document.writeln("<br/> Largest of two number using arrow style "+findLargest2(100,50))
