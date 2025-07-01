4fafdfbbb217487525643bcde9e26d3618c22316
Akash <akash300383@gmail.com>
2025-07-01 14:15:51 +0530

    done

---
+++ Course 4 - Develop a Web Application using frontend stack/JavaScript Using ES6/stringObject.js
@@ -0,0 +1,10 @@
+let msg1 = "Welcome to JavaScript";
+let msg2 = new String("Welcome to JavaScript");
+document.writeln("<br/>"+msg1)
+document.writeln("<br/>"+msg2)
+document.writeln("<br/>"+msg1.length)
+document.writeln("<br/>"+msg1.toUpperCase())
+document.writeln("<br/>"+msg1.toLowerCase())
+document.writeln("<br/>"+msg1.substring(3))
+document.writeln("<br/>"+msg1.substring(3,10))
+document.writeln("<br/>"+msg1.replace("JavaScript","Java"))
