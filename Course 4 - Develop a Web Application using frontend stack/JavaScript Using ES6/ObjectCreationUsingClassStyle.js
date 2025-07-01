bd9dfd0ef73ad534e0e29de8430590cffd19862e
Akash <akash300383@gmail.com>
2025-07-01 16:55:01 +0530

    done

---
+++ Course 4 - Develop a Web Application using frontend stack/JavaScript Using ES6/ObjectCreationUsingClassStyle.js
@@ -0,0 +1,21 @@
+class Employee {
+    id=100;
+    name = "Raj"
+    salary = 12000;
+
+    displayEmployeeInfo() {
+        document.writeln("<br/> Employee details in display employee function ")
+        document.writeln("<br/> id is "+this.id)
+        document.writeln("<br/> id is "+this.name);
+        document.writeln("<br/> id is "+this.salary);
+    }
+
+}
+
+let emp1 = new Employee();
+document.writeln(emp1)
+document.writeln("<br/> Employee Details ")
+document.writeln("<br/> id is "+emp1.id)
+document.writeln("<br/> name is "+emp1.name)
+document.writeln("<br/> salary is "+emp1.salary)
+emp1.displayEmployeeInfo();
--- Course 4 - Develop a Web Application using frontend stack/JavaScript Using ES6/ObjectCreationUsingFunctionStyle.js
+++ Course 4 - Develop a Web Application using frontend stack/JavaScript Using ES6/ObjectCreationUsingFunctionStyle.js
@@ -1,9 +1,38 @@
 // simple object creation in function style 
-function Employee() {
-    this.id =100;           // instance variable 
-    this.name = "Ravi";     // instance variable 
+// function Employee() {
+//     this.id =100;           // instance variable 
+//     this.name = "Ravi";     // instance variable 
+//     let age = 21;           // it is local variable consider 
+//     var designation = "Tester"; // it is also local variable consider 
+//     this.display = function() {             // employee object function 
+//         document.writeln("<br/>Employee details inside a Employee object")
+//         document.writeln("<br/> id is "+this.id)
+//         document.writeln("<br/> name is "+this.name)
+//         document.writeln("<br/> age is "+age)
+//         document.writeln("<br/> Designation is "+designation)
+//     }
+// }
+// //Employee();     // now consider as normal function 
+// let emp1 = new Employee();  // now consider as object 
+// let emp2 = new Employee();
+// document.writeln(emp1)
+// console.log(emp1)
+// //console.log(Employee)
+// document.writeln("Employee details")
+// document.writeln("<br/> id is "+emp1.id)
+// document.writeln("<br/> name is "+emp1.name)
+// document.writeln("<br/> age is "+emp1.age)
+// document.writeln("<br/> Designation is "+emp1.designation)
+// emp1.display();
+// emp1.display();
+
+
+// creating object of employee with parameter constructor style 
+function Employee(id,name,designation) {
+    this.id =id;           // instance variable 
+    this.name = name;     // instance variable 
     let age = 21;           // it is local variable consider 
-    var designation = "Tester"; // it is also local variable consider 
+    var designation = designation; // it is also local variable consider 
     this.display = function() {             // employee object function 
         document.writeln("<br/>Employee details inside a Employee object")
         document.writeln("<br/> id is "+this.id)
@@ -12,15 +41,7 @@ function Employee() {
         document.writeln("<br/> Designation is "+designation)
     }
 }
-//Employee();     // now consider as normal function 
-let emp1 = new Employee();  // now consider as object 
-document.writeln(emp1)
-console.log(emp1)
-//console.log(Employee)
-document.writeln("Employee details")
-document.writeln("<br/> id is "+emp1.id)
-document.writeln("<br/> name is "+emp1.name)
-document.writeln("<br/> age is "+emp1.age)
-document.writeln("<br/> Designation is "+emp1.designation)
+let emp1 = new Employee(100,"Ravi","Tester")
+let emp2 = new Employee(101,"Raju","Developer")
 emp1.display();
-
+emp2.display();
