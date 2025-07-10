9b953a37220645af19c488c6be5b9f72e18e725e
Akash <akash300383@gmail.com>
2025-07-10 15:11:40 +0530

    done

--- Course 4 - Develop a Web Application using frontend stack/React JS Examples/react-routing-demo-app/src/Login.js
+++ Course 4 - Develop a Web Application using frontend stack/React JS Examples/react-routing-demo-app/src/Login.js
@@ -1,18 +1,34 @@
+import axios from "axios";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function Login() {
+    let URL = "http://localhost:3000/logins";
 let [login,setLogin]=useState({emailId:"",password:""})
+let [loginsDbData,setLoginsDbData]=useState([]);
 let navigate = useNavigate();
 let handleLogin = (event)=> {
     event.preventDefault();
     //console.log(login);
     // login details we need to pass to backend technologies or we can check from json file with help of axios tools. 
-    if(login.emailId==="admin@gmail.com" && login.password==="admin@123"){
+    
+    // if(login.emailId==="admin@gmail.com" && login.password==="admin@123"){
+    //         alert("successfully login")
+    //         navigate("/home");
+    // }else {
+    //         alert("failure try once again")
+    // }
+    // please check login details with json file using axios tools. 
+
+    axios.get(URL).then(result=>setLoginsDbData(result.data)).catch(error=>console.log(error))
+
+    let result = loginsDbData.find(ll=>ll.emailid===login.emailId && ll.password===login.password);
+    //console.log(result);
+    if(result==undefined){
+            alert("emailId or password wrong")
+    }else {
             alert("successfully login")
             navigate("/home");
-    }else {
-            alert("failure try once again")
     }
     setLogin({emailId:"",password:""});
 }
